7b1bebe6c2e3d3912c9dd3611c5edd1b5f89ef99
kamiyo <kamiyo@gmail.com>
2018-12-24 12:39:57 -0600

    added mui

--- src/musicmath/main.cljs
+++ src/musicmath/main.cljs
@@ -1,12 +1,18 @@
 (ns musicmath.main
   (:require [reagent.core :as r]
-            [stylefy.core :as css]))
+            [re-frame.core :refer [dispatch dispatch-sync]]
+            [stylefy.core :as stylefy]
+            [musicmath.styles]))
 
 (defn view []
   [:div {:className "whoa"} "What's up?"])
 
 (defn main! []
   (println "[main]: entered!")
+  (stylefy/init
+   {:global-vendor-prefixes
+    {::stylefy/vendors ["webkit" "moz" "o"]
+     ::stylefy/auto-prefix #{:border-radius :box-shadow :appearance :user-select}}})
   (r/render [view]
             (.getElementById js/document "app")))
 
---
+++ src/musicmath/styles.cljs
@@ -0,0 +1,4 @@
+(ns musicmath.styles
+  (:require ["@material-ui/core" :refer [createMuiTheme]]))
+
+(def my-blue {:rgb "rgb(78, 134, 164)" :hex "#4e86a4"})
